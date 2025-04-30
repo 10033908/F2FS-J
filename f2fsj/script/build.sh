2bb4a002be4906950ff79fe1bf070dec5290eb0d
CYT <49577655+10033908@users.noreply.github.com>
2025-04-30 23:47:58 +0800

    Add F2FSJ code
    Src codes

---
+++ f2fsj/script/build.sh
@@ -0,0 +1,45 @@
+make_command=$1
+
+function make_vfs_ko(){
+    #make $MODULE_NAME LLVM=y # compile with clang
+    make $MODULE_NAME # compile with gcc
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mko file compile success\e[0m"
+        sudo cp ./build/lib/modules/5.15.39/extra/f2fsj.ko /lib/modules/5.15.39/f2fsj.ko
+    return 0
+
+    else
+        echo "make f2fsj.ko fail"
+        return 1
+    fi
+}
+
+function make_clean(){
+    make clean
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mmake clean over \e[0m"
+        return 0
+    else
+        echo "make clean happen exception"
+        return 0
+    fi
+}
+
+##############################
+if [ $make_command == "f2fsj" ]; then
+    make_vfs_ko
+    if [ $? -ne 0 ]; then
+        exit
+    fi
+    make_clean
+    if [ $? -ne 0 ]; then
+        exit
+    fi
+    echo -e "\e[32m***************************************\e[0m"
+    echo -e "\e[32m*       make f2fsj succuss            *\e[0m"
+    echo -e "\e[32m*                                     *\e[0m"
+    echo -e "\e[32m***************************************\e[0m"
+    echo -e $(date)
+elif [ $make_command == "-h" ]; then
+    echo "./script/build.sh f2fsj, make f2fsj.ko"
+fi
